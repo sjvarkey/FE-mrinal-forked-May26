0cf0da1404329cbb7c23cebf6a15d7562b160abe
mrinal1224 <iammrinalkc@gmail.com>
2025-06-16 22:38:05 +0530

    var keyword and datatypes

---
+++ Class-8(Introduction to JS)/variables.js
@@ -0,0 +1,29 @@
+// Variables in JS
+// loosely type langauge
+
+var a = 20; // number
+
+var b = "scaler"; // string
+var f = "a"; // string
+
+var c = true; // boolean
+
+var g; // undefined
+
+var h = null;
+
+var d = 12.123; // number
+
+console.log(typeof a , a);
+console.log(typeof b , b);
+console.log(typeof c , c);
+console.log(typeof d , d);
+console.log(typeof f , f);
+console.log(typeof g , g);
+console.log(typeof h , h);
+
+// Number - Int , Float , Double , decimal , power
+// String - char , string
+// Boolean - true and false
+// Null
+// Undefined
