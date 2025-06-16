ad9df615b0e65aaa166a72dc6c183efb7c5f8d44
mrinal1224 <iammrinalkc@gmail.com>
2025-06-16 23:00:36 +0530

    variables keywords

---
+++ Class-8(Introduction to JS)/es6.js
@@ -0,0 +1,37 @@
+var a = 20
+
+console.log(a)
+
+
+// var a = 'scaler'
+a = 40
+
+console.log(a)
+
+// This var keyowrd is allowing redeclartion of a varibale with the same name
+
+// let keyword
+
+let b = 15
+console.log(b)
+
+b = 19
+
+console.log(b)
+
+
+// for(let i=0 ; i<10 ; i++){
+//     console.log(i)
+// }
+
+// const
+
+const c = 10
+
+c = 20
+console.log(c)
+
+
+
+
+
