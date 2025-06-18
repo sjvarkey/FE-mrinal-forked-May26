11736ba3ef710b43023e534a1303cab3310564f4
mrinal1224 <iammrinalkc@gmail.com>
2025-06-18 21:29:23 +0530

    push

---
+++ Class-9 (Objects functions and Execution)/object.js
@@ -0,0 +1,22 @@
+// Object are just key value pair of a speific Enitity
+
+// car
+// person
+// city
+
+// car - color , company , name , topSpeed
+// person - name , gender , age
+// city - name , state , country , pincode
+
+// create a simple object of a car
+
+let car = {
+    color : 'Red',
+    name : 'Bugatti',
+    topSpeed : '250km/hr',
+    isGeared : false,
+    doors : 2
+
+}
+
+console.log(car)
