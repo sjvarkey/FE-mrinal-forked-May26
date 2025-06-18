b87a529a619312df15ec5294a8087eae0d9e0fbf
mrinal1224 <iammrinalkc@gmail.com>
2025-06-18 22:01:14 +0530

    objects

--- Class-9 (Objects functions and Execution)/object.js
+++ Class-9 (Objects functions and Execution)/object.js
@@ -10,13 +10,77 @@
 
 // create a simple object of a car
 
-let car = {
-    color : 'Red',
-    name : 'Bugatti',
-    topSpeed : '250km/hr',
-    isGeared : false,
-    doors : 2
+// let car = {
+//     color : 'Red',
+//     name : 'Bugatti',
+//     topSpeed : '250km/hr',
+//     isGeared : false,
+//     doors : 2
+// }
 
+
+
+// console.log(car)
+
+// Create an Object for Captain America
+
+let cap = {
+  name: "Steve Rogers",
+  age: 105,
+
+  friends: ["Bucky", "Bruce Banner", "Tony Stark"],
+
+  address: {
+    country: "USA",
+    city: {
+      name: "Brooklyn",
+      pincode: 123456,
+    },
+  },
+
+  isAvenger: true,
 }
 
-console.log(car)
+// How to access object properties and how to maipulate them
+
+// access properties - dot notation , bracket notation
+
+// dot notation
+
+console.log(cap.age)
+console.log(cap.friends)
+
+console.log(cap.address.city.name)
+
+// bracket notation
+console.log(cap['address']['country'])
+
+
+
+
+// manipulate values
+
+// add captain america movies
+cap.movies = ['civil war' , 'infinity war', 'winter soldier']
+
+cap.isAvenger = false
+
+
+
+// delete props from object
+
+delete cap.age
+
+console.log(cap)
+
+
+
+
+
+
+
+
+
+
+
+
