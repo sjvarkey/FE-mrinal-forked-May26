15d4fb39ef5aefd89d6064413c3343b334ace7c3
mrinal1224 <iammrinalkc@gmail.com>
2025-06-23 22:19:06 +0530

    push

---
+++ Class-11(Problem Solving Session)/4.js
@@ -0,0 +1,33 @@
+// Create an array of book objects where each book has:
+
+// { title: "Book Name", author: "Author Name", isAvailable: true }
+
+// Write functions to:
+//     List all books.
+//     Mark a book as borrowed (isAvailable = false).
+//     Return a book (isAvailable = true).
+//     Find books by a specific author.
+
+const books = [
+    { title: "Book1", author: "Author1", isAvailable: true },
+    { title: "Book2", author: "Author2", isAvailable: true },
+    { title: "Book3", author: "Author3", isAvailable: true },
+    { title: "Book4", author: "Author4", isAvailable: true },
+    { title: "Book5", author: "Author5", isAvailable: true },
+  ];
+  
+  function listBooks() {
+    // Write your code here
+  }
+  
+  function markAsBorrowed(title) {
+    // Write your code here
+  }
+  
+  function returnBook(title) {
+    // Write your code here
+  }
+  
+  function findBooksByAuthor(author) {
+    // Write your code here
+  }
