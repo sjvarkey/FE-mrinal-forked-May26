30cf0d9dfffd9334e5eeee62784b9276ee69c6ee
mrinal1224 <iammrinalkc@gmail.com>
2025-06-23 21:58:31 +0530

    push

---
+++ Class-11(Problem Solving Session)/3.js
@@ -0,0 +1,30 @@
+const students = [
+    { name: "Alice", score: 92 },
+    { name: "Bob", score: 80 },
+    { name: "Charlie", score: 60 },
+    {name : "Sumit" , score : 97},
+    {name : "Mrinal" , score : 10},
+  ];
+
+
+// "A" (90-100)
+// "B" (75-89)
+// "C" (below 75)
+
+// Return an object like:
+// {
+//   A: ["Alice" , 'Sumit'],
+//   B: ["Bob"],
+//   C: ["Charlie", "Mrinal"]
+// }
+
+
+function categorizeStudents(studentList) {
+    // Write your code here
+  }
+  
+  console.log(categorizeStudents(students));
+
+
+
+
