6cb3526df40420e833a66a78cfcd3d434532f82a
mrinal1224 <iammrinalkc@gmail.com>
2025-06-23 22:18:07 +0530

    Array of objectes

--- Class-11(Problem Solving Session)/3.js
+++ Class-11(Problem Solving Session)/3.js
@@ -1,15 +1,16 @@
 const students = [
-    { name: "Alice", score: 92 },
-    { name: "Bob", score: 80 },
-    { name: "Charlie", score: 60 },
-    {name : "Sumit" , score : 97},
-    {name : "Mrinal" , score : 10},
-  ];
-
+  { name: "Alice", score: 92 },
+  { name: "Bob", score: 80 },
+  { name: "Charlie", score: 60 },
+  { name: "Sumit", score: 97 },
+  { name: "Mrinal", score: 10 },
+];
 
 // "A" (90-100)
 // "B" (75-89)
 // "C" (below 75)
+
+// Interview Question of a startup - coditation System
 
 // Return an object like:
 // {
@@ -18,13 +19,20 @@ const students = [
 //   C: ["Charlie", "Mrinal"]
 // }
 
-
 function categorizeStudents(studentList) {
-    // Write your code here
+  const categories = { A: [], B: [], C: [] };
+
+  for (let i = 0; i < studentList.length; i++) {
+    if (studentList[i].score >= 90) {
+      categories.A.push(studentList[i].name);
+    } else if (studentList[i].score >= 75) {
+      categories.B.push(studentList[i].name);
+    } else {
+      categories.C.push(studentList[i].name);
+    }
   }
-  
-  console.log(categorizeStudents(students));
 
+  return categories;
+}
 
-
-
+console.log(categorizeStudents(students));
