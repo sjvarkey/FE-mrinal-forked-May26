66df5116b9c23659fa0c90fa9511cf151956d0e2
mrinal1224 <iammrinalkc@gmail.com>
2025-06-23 22:47:49 +0530

    push

--- Class-11(Problem Solving Session)/4.js
+++ Class-11(Problem Solving Session)/4.js
@@ -9,25 +9,56 @@
 //     Find books by a specific author.
 
 const books = [
-    { title: "Book1", author: "Author1", isAvailable: true },
-    { title: "Book2", author: "Author2", isAvailable: true },
-    { title: "Book3", author: "Author3", isAvailable: true },
-    { title: "Book4", author: "Author4", isAvailable: true },
-    { title: "Book5", author: "Author5", isAvailable: true },
-  ];
-  
-  function listBooks() {
-    // Write your code here
+  { title: "Book1", author: "Author1", isAvailable: true },
+  { title: "Book2", author: "Author2", isAvailable: true },
+  { title: "Book3", author: "Author3", isAvailable: true },
+  { title: "Book4", author: "Author4", isAvailable: true },
+  { title: "Book5", author: "Author5", isAvailable: true },
+  { title: "Book6", author: "Author1", isAvailable: true },
+  { title: "Book7", author: "Author1", isAvailable: true },
+];
+
+function listBooks() {
+  return books;
+}
+
+function markAsBorrowed(title) {
+  for (let book of books) {
+    if (book.title == title) {
+      if (book.isAvailable == true) {
+        book.isAvailable = false;
+        return book;
+      }
+    }
   }
-  
-  function markAsBorrowed(title) {
-    // Write your code here
+
+  return "Book Not Found";
+}
+
+function returnBook(title) {
+  for (let book of books) {
+    if (book.title == title) {
+      if (book.isAvailable == false) {
+        book.isAvailable = true;
+        return book;
+      }
+    }
   }
-  
-  function returnBook(title) {
-    // Write your code here
+
+  return "Book Not Found";
+}
+
+function findBooksByAuthor(author) {
+  const authoredbooks = [];
+  for (let book of books) {
+    if (book.author == author) {
+      authoredbooks.push(book);
+    }
   }
-  
-  function findBooksByAuthor(author) {
-    // Write your code here
-  }
+  return authoredbooks;
+}
+
+console.log(listBooks());
+console.log(markAsBorrowed("Book1"));
+console.log(returnBook("Book1"));
+console.log(findBooksByAuthor("Author1"));
