baac4d9d3fd16cbe97679952aca291bdce5dab51
mrinal1224 <iammrinalkc@gmail.com>
2025-06-25 21:31:27 +0530

    push

---
+++ Class-12(Functional Programming)/index.js
@@ -0,0 +1,23 @@
+// callback functions
+
+function printName(firstName , cb , cb2){
+    console.log(firstName) // Rishav
+    cb(24) // 24
+    cb2('Ghosh')
+   
+}
+
+
+function printLastName(lastName){
+  console.log(lastName)
+}
+
+function printAge(age){
+    console.log(age)
+}
+
+printName('Rishav',printAge , printLastName )
+
+
+
+
