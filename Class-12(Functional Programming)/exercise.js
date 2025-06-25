07cae2367101e87ecf7c91a32cc13e6a0d914a55
mrinal1224 <iammrinalkc@gmail.com>
2025-06-25 21:42:33 +0530

    push

---
+++ Class-12(Functional Programming)/exercise.js
@@ -0,0 +1,9 @@
+let myRadiusArray = [2, 3, 4, 5, 8]
+
+// write functions that will calculate area , 
+// circumference and diameter of these circles
+// you have to return a new array 
+
+// 3 functions
+
+// you have 7-8 mins to do this
--- Class-12(Functional Programming)/index.js
+++ Class-12(Functional Programming)/index.js
@@ -1,23 +1,18 @@
 // callback functions
 
-function printName(firstName , cb , cb2){
-    console.log(firstName) // Rishav
-    cb(24) // 24
-    cb2('Ghosh')
-   
+function printName(firstName, cb, cb2) {
+  console.log(firstName); // Rishav
+  cb(24); // 24
+
+  cb2("Ghosh");
 }
 
-
-function printLastName(lastName){
-  console.log(lastName)
+function printLastName(lastName) {
+  console.log(lastName);
 }
 
-function printAge(age){
-    console.log(age)
+function printAge(age) {
+  console.log(age);
 }
 
-printName('Rishav',printAge , printLastName )
-
-
-
-
+printName("Rishav", printAge, printLastName);
