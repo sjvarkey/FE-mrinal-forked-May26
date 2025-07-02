e97687af0824832d9eaa173ae2851ca89012fa3d
mrinal1224 <iammrinalkc@gmail.com>
2025-07-02 23:17:33 +0530

    weather app

--- Class-15(Weather App)/weatherApp/script.js
+++ Class-15(Weather App)/weatherApp/script.js
@@ -1,7 +1,13 @@
 const form = document.querySelector('form')
 const searchField = document.querySelector('.searchField')
+const tempratureField = document.querySelector('.temp')
+const cityField = document.querySelector('.time_location p')
+const dateField = document.querySelector(".time_location span");
+const emojiField = document.querySelector(".weather-icon img");
+const weatherField = document.querySelector(".weather_condition span");
+let target = 'London'
 
-let target = 'Mumbai'
+
 
 
 form.addEventListener('submit' , search)
@@ -16,11 +22,38 @@ function search(e){
 }
 
 
+
 async function fetchData(target){
-     const endPoint = `http://api.weatherapi.com/v1/current.json?key=35af7ff606db422880d141328231305&q=${target}&aqi=no`
-     const response = await fetch(endPoint)
-     const data = await response.json()
+     let endPoint = `http://api.weatherapi.com/v1/current.json?key=35af7ff606db422880d141328231305&q=${target}&aqi=no`
+     let response = await fetch(endPoint)
+     let data = await response.json()
 
      console.log(response)
      console.log(data)
+
+     let currTemp = data.current.temp_c
+     let cityName = data.location.name
+     let localTime = data.location.localtime
+     let condition = data.current.condition.text
+     let conditionIcon = data.current.condition.icon
+
+     updateWeather(currTemp , cityName , localTime , condition , conditionIcon)
 }
+
+function updateWeather(currTemp , cityName , localTime , condition , conditionIcon){
+    tempratureField.innerText = currTemp + '°' 
+    cityField.innerText = cityName
+    dateField.innerText = localTime
+    weatherField.innerText = condition
+    emojiField.src = conditionIcon
+
+    
+
+    searchField.value=' '
+
+    
+
+
+}
+
+fetchData(target)
