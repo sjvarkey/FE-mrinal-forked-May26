4809f57f0a653b09a821db93f5eec1aa9c330891
mrinal1224 <iammrinalkc@gmail.com>
2025-07-23 22:24:17 +0530

    push

--- Class-18(promises)/readFileP.js
+++ Class-18(promises)/readFileP.js
@@ -11,12 +11,30 @@ const fileSystem = require("fs");
 // file operations with Promises
 
 let readFilePromise1 = fileSystem.promises.readFile("f1.txt");
+let readFilePromise2 = fileSystem.promises.readFile("f2.txt");
+let readFilePromise3 = fileSystem.promises.readFile("f3.txt");
 
 // then and catch
 
 readFilePromise1
   .then(function (data) {
-    console.log("This is file Data -> " + data);
+    console.log("This is file Data 1 -> " + data);
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+
+readFilePromise2
+  .then(function (data) {
+    console.log("This is file Data 2 -> " + data);
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+
+readFilePromise3
+  .then(function (data) {
+    console.log("This is file Data 3 -> " + data);
   })
   .catch(function (err) {
     console.error(err);
