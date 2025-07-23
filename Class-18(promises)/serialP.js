162919440e8e59d7bf59e57c54407d0f2cec2ff7
mrinal1224 <iammrinalkc@gmail.com>
2025-07-23 23:27:32 +0530

    push

--- Class-18(promises)/serialP.js
+++ Class-18(promises)/serialP.js
@@ -1,6 +1,6 @@
 const fileSystem = require("fs");
 
-console.log("start")
+console.log("start");
 
 // fileSystem.readFile("f1.txt", function (err, data) {
 //   if (err) {
@@ -13,33 +13,23 @@ console.log("start")
 // file operations with Promises
 
 let readFilePromise1 = fileSystem.promises.readFile("f1.txt");
-let readFilePromise2 = fileSystem.promises.readFile("f2.txt");
-let readFilePromise3 = fileSystem.promises.readFile("f3.txt");
 
 // then and catch
 
 readFilePromise1
   .then(function (data) {
     console.log("This is file Data 1 -> " + data);
+    return fileSystem.promises.readFile("f2.txt");
+  })
+  .then(function (data2) {
+    console.log("this is file data2 -> " + data2);
+    return fileSystem.promises.readFile("f3.txt");
+  })
+  .then(function (data3) {
+    console.log("this is file data3 -> " + data3);
   })
   .catch(function (err) {
     console.error(err);
   });
 
-readFilePromise2
-  .then(function (data) {
-    console.log("This is file Data 2 -> " + data);
-  })
-  .catch(function (err) {
-    console.error(err);
-  });
-
-readFilePromise3
-  .then(function (data) {
-    console.log("This is file Data 3 -> " + data);
-  })
-  .catch(function (err) {
-    console.error(err);
-  });
-
-console.log('End')
+console.log("End");
