2608f5700ce0084416e68845ebc994de9fbdc34f
mrinal1224 <iammrinalkc@gmail.com>
2025-07-25 21:58:56 +0530

    push

--- Class-19 (async and Await)/asyncAwait.js
+++ Class-19 (async and Await)/asyncAwait.js
@@ -4,6 +4,12 @@ const p1 = new Promise(function (resolve, reject) {
   }, 5000);
 });
 
+const p2 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve("This is resolved data bye bye");
+    }, 1000);
+  });
+
 function resolvePromise() {
   p1.then((res) => {
     console.log(res);
@@ -11,11 +17,24 @@ function resolvePromise() {
 
   console.log("Scaler");
 }
+//Scaler
+// This is your resolved Data
 
  async function handlePromise(){
-   let data = await p1
+  console.log('Hellllo')
+
+   let data = await p1 // stops here
+   console.log('Scaler')
    console.log(data)
 }
 
-// resolvePromise();
-handlePromise()
+async function printBye(){
+   let data2= await p2
+   console.log(data2)
+}
+
+//   resolvePromise(); // Promise with then
+  handlePromise() // async await
+  printBye()
+  
+  
