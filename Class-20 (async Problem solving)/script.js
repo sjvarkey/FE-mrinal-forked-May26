2e41404f181c2accfc03b2a74cb2b2247bea4eab
mrinal1224 <iammrinalkc@gmail.com>
2025-07-28 21:12:59 +0530

    Q1

---
+++ Class-20 (async Problem solving)/script.js
@@ -0,0 +1,24 @@
+// Q1 Simulate a Delayed Task (Using setTimeout & Callbacks)
+// Create a function fakeDownload(url, callback) 
+//that simulates downloading a file from a URL. 
+//The function should:
+
+// Log "Downloading from [url]...".
+// Wait 2 seconds.
+// Call the callback function with "Download complete!".
+
+
+// Task:
+// Create a function fakeDownload(url, callback) that simulates downloading a file from a URL. The function should:
+//     Log "Downloading from [url]...".
+//     Wait 2 seconds.
+//     Call the callback function with "Download complete!".
+
+function fakeDownload(url, callback) {
+    // Write code here...
+  }
+  
+  // Usage:
+  fakeDownload("https://example.com/file", (message) => {
+    console.log(message); // Expected Output: "Download complete!" (after 2 sec)
+  }); 
