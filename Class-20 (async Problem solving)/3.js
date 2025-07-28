1527e9a396162f1e2425aa171a2a8568300fe3b4
mrinal1224 <iammrinalkc@gmail.com>
2025-07-28 22:19:56 +0530

    3rd solution

--- Class-20 (async Problem solving)/3.js
+++ Class-20 (async Problem solving)/3.js
@@ -6,7 +6,28 @@
 //     Country
 // Handle API errors and provide appropriate feedback if the fetch fails.
 
+let fetchBtn = document.getElementById('fetchUserBtn')
+
+fetchBtn.addEventListener('click' , fetchRandomUser )
+
+
 
 async function fetchRandomUser() {
-    // Write code here...
-  }
+     try {
+
+        const response = await fetch(`https://randomuser.me/api/`)
+        const data = await response.json()
+        let userData = data.results[0]
+
+        document.getElementById('userName').innerText= `Name : ${userData.name.first}`
+        document.getElementById('userEmail').innerText= `Email : ${userData.email}`
+        document.getElementById('userCountry').innerText= `Country : ${userData.location.country}`
+
+        document.getElementById('userCard').style.display='block'
+
+     
+        
+     } catch (error) {
+        
+     }
+}
