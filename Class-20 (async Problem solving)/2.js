8df7a818c759ef938b1c02308898a8bdaf82b425
mrinal1224 <iammrinalkc@gmail.com>
2025-07-28 21:27:24 +0530

    2nd question

---
+++ Class-20 (async Problem solving)/2.js
@@ -0,0 +1,10 @@
+// Task:
+// Fetch a random cat fact from "https://catfact.ninja/fact" and log it.
+// Use promise chaining to handle the fetch request. Log any errors that occur.
+
+function fetchCatFact() {
+    // Write code here...
+  }
+  
+  // Usage:
+fetchCatFact();
--- Class-20 (async Problem solving)/script.js
+++ Class-20 (async Problem solving)/script.js
@@ -10,15 +10,18 @@
 
 // Task:
 // Create a function fakeDownload(url, callback) that simulates downloading a file from a URL. The function should:
-//     Log "Downloading from [url]...".
+//     Log `Downloading from ${url}...`.
 //     Wait 2 seconds.
 //     Call the callback function with "Download complete!".
 
 function fakeDownload(url, callback) {
-    // Write code here...
+    console.log(`Downloading from ${url}...`)
+    setTimeout(()=>{
+        callback(`Download complete!`)
+    } , 2000)
   }
   
   // Usage:
-  fakeDownload("https://example.com/file", (message) => {
-    console.log(message); // Expected Output: "Download complete!" (after 2 sec)
+  fakeDownload("https://example.com/file", function(message){
+    console.log(message)
   }); 
