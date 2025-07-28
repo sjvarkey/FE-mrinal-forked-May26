7b92f632da625443aa648887a999effb45edb10a
mrinal1224 <iammrinalkc@gmail.com>
2025-07-28 23:05:52 +0530

    solution for 4th problem

--- Class-20 (async Problem solving)/4.js
+++ Class-20 (async Problem solving)/4.js
@@ -1,8 +1,39 @@
-// Build a simple dog image gallery 
-// that fetches three random dog images 
-// from "https://dog.ceo/api/breeds/image/random" 
+// Build a simple dog image gallery
+// that fetches three random dog images
+// from "https://dog.ceo/api/breeds/image/random"
 // in parallel using a Promise method.
-// Clicking the "Get Dogs" button should display 
+// Clicking the "Get Dogs" button should display
 // three new random images in the UI.
 // Clear previous images before displaying new ones.
 // Handle errors if fetching fails.
+
+let getImages = document.getElementById("fetchDogsBtn");
+
+getImages.addEventListener("click", fetchDogs);
+
+async function fetchDogs() {
+  try {
+    const urls = [
+      "https://dog.ceo/api/breeds/image/random",
+      "https://dog.ceo/api/breeds/image/random",
+      "https://dog.ceo/api/breeds/image/random",
+    ];
+
+    const responses = await Promise.all(urls.map((url) => fetch(url)));
+    const data = await Promise.all(responses.map((res) => res.json()));
+
+    console.log(data);
+
+    const container = document.getElementById("dogContainer");
+
+    container.innerHTML=''
+
+    data.forEach((dog) => {
+      let image = document.createElement("img");
+      image.src = dog.message;
+      container.appendChild(image);
+    });
+  } catch (error) {
+    alert("Error fetching dog images: " + error.message);
+  }
+}
