9e98e367913a14f41fc946331662f3ee600e172e
mrinal1224 <iammrinalkc@gmail.com>
2025-07-28 22:24:51 +0530

    push 4th question

---
+++ Class-20 (async Problem solving)/4.js
@@ -0,0 +1,8 @@
+// Build a simple dog image gallery 
+// that fetches three random dog images 
+// from "https://dog.ceo/api/breeds/image/random" 
+// in parallel using a Promise method.
+// Clicking the "Get Dogs" button should display 
+// three new random images in the UI.
+// Clear previous images before displaying new ones.
+// Handle errors if fetching fails.
