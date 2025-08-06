7e3bd2e9d5f32e8cb89fca37d43547f911f528d0
mrinal1224 <iammrinalkc@gmail.com>
2025-08-06 22:03:04 +0530

    node non strict

---
+++ Class-24 (this keyword)/thisNode.js
@@ -0,0 +1,41 @@
+// 1. this in global
+
+// console.log(this)
+
+// 2. this in function
+
+function test() {
+  // console.log(this) // global object
+}
+
+test();
+
+// 3. This inside an Object
+
+var obj = {
+    name : 'Adam',
+    age : 26,
+    fn: function(){
+        console.log(this.name)
+    }
+}
+
+ obj.fn()
+
+var obj2 = {
+  name: "Adam",
+  age: 26,
+
+  fn: function () {
+      let nestedFn = function(){
+        console.log(this.name)
+      }
+      return nestedFn
+  },
+};
+
+let fn2 = obj2.fn(); // nestedFn
+
+console.log(fn2);
+
+fn2();
