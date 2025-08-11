918a5a884c116c5b979f3d8056f2b21effba3867
mrinal1224 <iammrinalkc@gmail.com>
2025-08-11 21:53:08 +0530

    push

---
+++ Class-26(Prototype and Prototypal inheritnace)/script.js
@@ -0,0 +1,30 @@
+function Car(modelPara, yearPara, colorPara) {
+  this.model = modelPara;
+  this.year = yearPara;
+  this.color = colorPara;
+}
+
+Car.prototype.displayInfo = function () {
+  console.log(`This is a ${this.color}  ${this.year} ${this.model} `);
+};
+
+let car1 = new Car("Toyota Camry", 2020, "White");
+console.log(car1);
+let car2 = new Car("Lexus", 2021, "Black");
+console.log(car2);
+
+let car3 = new Car("BMW x6", 2022, "Red");
+console.log(car3);
+
+car1.displayInfo()
+car2.displayInfo()
+car3.displayInfo()
+// car1.displayInfo()
+
+// let car2 = {
+//   model: "BMW X6",
+//   year: 2020,
+//   color: "red",
+// };
+
+// console.log(car2);
