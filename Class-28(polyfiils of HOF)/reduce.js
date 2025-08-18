97255e252dc91e9ec42ae41f86d49164b4afcfe4
mrinal1224 <iammrinalkc@gmail.com>
2025-08-18 23:17:43 +0530

    reduce polyfill

--- Class-28(polyfiils of HOF)/reduce.js
+++ Class-28(polyfiils of HOF)/reduce.js
@@ -8,38 +8,49 @@
 
 // 5 . if accumulator is not initizled
 
-let arr = [1 ,2 ,3 ,4 ,5]
+// let arr = [1, 2, 3, 4, 5];
 
+// let sum = arr.reduce(function (acc, curr) {
+//   acc = acc + curr;
+//   return acc;
+// }, 0);
 
-let sum = arr.reduce(function(acc,curr){
-    acc = acc+curr
-    return acc
-},0)
-
-console.log(sum)
+// console.log(sum);
 
 // Polyfill fof Reduce
 
-Array.prototype.myReduce = function(cb , initialValue){
-
-  let accumulator=initialValue // 0
-
-
-  for(let i=0 ; i<this.length ; i++){
-        accumulator = cb(accumulator , this[i])
+Array.prototype.myReduce = function (cb, initialValue) {
+  if (this == null || this == undefined) {
+    throw new TypeError("Array.prototype.myFilter called on null or undefined");
   }
 
-  return accumulator
+  if (typeof callback !== "function") {
+    throw new TypeError(callback + " is not a function");
+  }
 
-}
+  if (Array.isArray(this) === true) {
+    let accumulator;
+    let firstIndex;
 
-// let nums2 = [1 ,3 ,4 ,5]
+    if (arguments.length === 1) {
+      accumulator = this[0];
+      firstIndex = 1;
+    } else {
+      (accumulator = initialValue), (firstIndex = 0);
+    }
 
-// let sum2 = nums2.myReduce(function(acc , currValue){
-//     acc = acc+currValue
-//     return acc
-// }, )
-// console.log(sum2)
+    for (let i = firstIndex; i < this.length; i++) {
+      accumulator = cb(accumulator, this[i]);
+    }
 
+    return accumulator;
+  }
+};
 
+let nums2 = [1, 3, 4, 5];
 
+let sum2 = nums2.myReduce(function (acc, currValue) {
+  acc = acc + currValue;
+  return acc;
+}, 1);
+console.log(sum2);
