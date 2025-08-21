bb01860dd90edd030edb68f58e1d92a785b3d0e4
mrinal1224 <iammrinalkc@gmail.com>
2025-08-21 23:31:46 +0530

    any polyfill

--- Class-29(Promise Combinators)/anyPolyfill.js
+++ Class-29(Promise Combinators)/anyPolyfill.js
@@ -1,3 +1,26 @@
-// Polyfill Promises.any
+// Polyfill Promise.any
 
+Promise.myAny = function (subPromises) {
+  const promise = new Promise(function (resolve, reject) {
+    if (subPromises.length === 0)
+      reject(new AggregateError("No promises were provided."));
 
+    let rejections = [];
+    let rejectedCount = 0;
+
+    subPromises.forEach((subPromise, index) => {
+      Promise.resolve(subPromise)
+        .then(resolve) // resolve the promise immediately
+        .catch((err) => {
+          rejections[index] = err;
+          rejectedCount++;
+          if (rejectedCount == subPromises.length) {
+            reject(
+              new AggregateError(rejections, "All promises were rejected.")
+            );
+          }
+        });
+    });
+  });
+  return promise
+};
