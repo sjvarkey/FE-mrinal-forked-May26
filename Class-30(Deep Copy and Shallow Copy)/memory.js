1c6f088cfaf35e76515c89d5efaf01029e6a47cd
mrinal1224 <iammrinalkc@gmail.com>
2025-08-22 21:44:14 +0530

    stack and heaps

---
+++ Class-30(Deep Copy and Shallow Copy)/memory.js
@@ -0,0 +1,20 @@
+// let numOne = 50
+// let numTwo = 100
+// numTwo = numOne
+//  numOne = 200
+
+// console.log(numOne) // 200
+// console.log(numTwo) // 50
+
+
+const obj1 = {
+   name : 'John',
+   age : 25
+}
+
+const obj2 = obj1
+
+obj2.name = "Mark"
+
+console.log(obj1)  
+console.log(obj2) 
