8fcd1f4bf4886858e3053d82efe4fde091fa98db
mrinal1224 <iammrinalkc@gmail.com>
2025-09-01 22:08:27 +0530

    push

---
+++ LLD-3/Class-32(React Buiild Tools)/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+import './components/Navbar.css'
+
+
+function App() {
+  return (
+     <Navbar/>
+  )
+}
+
+export default App
---
+++ LLD-3/Class-32(React Buiild Tools)/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+
+
+function Navbar() {
+      return (
+        <nav className="navbar">
+          <div className="navbar-logo">MySite</div>
+          <ul className="navbar-links">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/services">Services</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      );
+    }
+
+export default Navbar
