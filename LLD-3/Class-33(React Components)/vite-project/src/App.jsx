6ead87066172b53c652062ba36cccd7e75156769
mrinal1224 <iammrinalkc@gmail.com>
2025-09-03 22:06:13 +0530

    push

--- LLD-3/Class-33(React Components)/vite-project/src/App.jsx
+++ LLD-3/Class-33(React Components)/vite-project/src/App.jsx
@@ -1,1 +1,13 @@
+import './App.css'
+import Counter from './components/Counter.jsx'
+import './components/Counter.css'
 
+function App() {
+ return (
+    <div>
+       <Counter/>
+    </div>
+  )
+}
+
+export default App
--- LLD-3/Class-33(React Components)/vite-project/src/components/Counter.jsx
+++ LLD-3/Class-33(React Components)/vite-project/src/components/Counter.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 function Counter() {
+    const [count , setCount] =useState(0)
+
   return (
-     
-  )
+    <div className="counter">
+      <button className="btn decrement">Decrement</button>
+      <h1 className="count">{count}</h1>
+      <button className="btn increment">Increment</button>
+
+      <button className="btn reset">Reset</button>
+    </div>
+  );
 }
 
-export default Counter
+export default Counter;
