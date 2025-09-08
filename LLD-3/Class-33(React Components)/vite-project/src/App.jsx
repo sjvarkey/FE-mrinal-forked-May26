40f303082ff8a413b65ab50cfd6acc56db6008fe
mrinal1224 <iammrinalkc@gmail.com>
2025-09-08 23:05:05 +0530

    push

--- LLD-3/Class-33(React Components)/vite-project/src/App.jsx
+++ LLD-3/Class-33(React Components)/vite-project/src/App.jsx
@@ -1,18 +1,18 @@
 import './App.css'
 import Counter from './components/Counter.jsx'
 import './components/Counter.css'
-import UserCard from './components/UserCard.jsx'
-import ProductList from './components/ProductList.jsx'
-import Form from './components/Form.jsx'
-import Thermometer from './components/Thermometer.jsx'
+
 
 function App() {
   console.log('App Rendered');
  return (
     <div>
-      <Thermometer/>
+     <Counter/>
     </div>
   )
 }
 
 export default App
+
+
+// Counter - 
--- LLD-3/Class-33(React Components)/vite-project/src/components/Counter.jsx
+++ LLD-3/Class-33(React Components)/vite-project/src/components/Counter.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 import ProductList from "./ProductList";
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
   function increment() {
     setCount(count + 1);
   }
-  console.log('Counter Rendered');
+
   function decrement() {
     if (count > 0) {
       setCount(count - 1);
@@ -19,24 +20,34 @@ function Counter() {
     setCount(0);
   }
 
+  useEffect(() => {
+    console.log("use Effect Runs");
+    document.title = `You clicked ${count} times`;
+  } , []);
+
   return (
     <div>
-    <div className="counter">
-      <button onClick={decrement} className="btn decrement">
-        Decrement
-      </button>
-      <h1 className="count">{count}</h1>
-      <button onClick={increment} className="btn increment">
-        Increment
-      </button>
+      <div className="counter">
+        <button onClick={decrement} className="btn decrement">
+          Decrement
+        </button>
+        <h1 className="count">{count}</h1>
+        <button onClick={increment} className="btn increment">
+          Increment
+        </button>
 
-      <button onClick={reset} className="btn reset">
-        Reset
-      </button>
+        <button onClick={reset} className="btn reset">
+          Reset
+        </button>
 
-     
-    </div>
-    
+        <label>Name</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <p>{name}</p>
+      </div>
     </div>
   );
 }
