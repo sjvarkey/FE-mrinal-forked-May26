6c955535255498e3540d016724dfc3eeaf057cd2
mrinal1224 <iammrinalkc@gmail.com>
2025-09-10 22:41:01 +0530

    push

--- LLD-3/Class-35(React-Router-Dom)/vite-project/src/App.jsx
+++ LLD-3/Class-35(React-Router-Dom)/vite-project/src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import About from "./components.jsx/About";
-import Contact from "./components.jsx/Contact";
-import Home from "./components.jsx/Home";
-import Navbar from "./components.jsx/Navbar";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
