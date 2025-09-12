d6a2f9b2554c9441c77482a251d5ce1f4a4c4ed3
mrinal1224 <iammrinalkc@gmail.com>
2025-09-12 21:56:44 +0530

    push

--- LLD-3/imdb/src/components/MovieCard.jsx
+++ LLD-3/imdb/src/components/MovieCard.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function MovieCard() {
   return (
     <div>
-        
+ 
+      <div className="flex justify-evenly">
+        <div
+          className="h-[40vh] w-[180px] bg-cover bg-center rounded-2xl  "
+          style={{
+            backgroundImage: `url(https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68)`,
+          }}
+        ></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default MovieCard
+export default MovieCard;
--- LLD-3/imdb/src/components/Movies.jsx
+++ LLD-3/imdb/src/components/Movies.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import Banner from './Banner'
+import React from "react";
+import Banner from "./Banner";
+import MovieCard from "./MovieCard";
 
 function Movies() {
   return (
     <div>
-        <Banner/>
+      <Banner />
+      <div className="text-2xl font-bold text-center m-5">
+        <h1>Trending Movies</h1>
+      </div>
+      <div className="flex flex-wrap gap-8 m-8">
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+         <MovieCard />
+          <MovieCard />
+           <MovieCard />
+            <MovieCard />
+            <MovieCard />
+              <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            
+      </div>
     </div>
-  )
+  );
 }
 
-export default Movies
+export default Movies;
