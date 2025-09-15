2ed51dce34b98d3baeccd7d0451b9c0838386ad8
mrinal1224 <iammrinalkc@gmail.com>
2025-09-15 22:54:35 +0530

    push

--- LLD-3/imdb/src/components/Banner.jsx
+++ LLD-3/imdb/src/components/Banner.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Banner() {
+
+  const [url , setUrl]= useState(null)
+
+ const imageurls = [ 'https://image.tmdb.org/t/p/original/1BpjcVIztlsk0mym7OLF6CW97eh.jpg',
+    'https://image.tmdb.org/t/p/original/zxi6WQPVc0uQAG5TtLsKvxYHApC.jpg',
+    'https://image.tmdb.org/t/p/original/28zX1DO1NJWeS1e573lSJQ9kiVh.jpg',]
+
+ useEffect(()=>{
+   function fetchImage(){
+    //  const url = 
+    // fetch and set a random url from the array
+    setUrl(url)
+  }
+  fetchImage()
+ }, [])
+
   return (
     <div
       className="relative h-[70vh] bg-cover bg-center flex items-center"
